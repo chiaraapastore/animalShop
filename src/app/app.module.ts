67af7053b1a9ac5b3a8e26428b3e3f8cdbbdb4e3
chiaraapastore <chiarapastoree001@gmail.com>
2024-11-04 11:40:09 +0100

    Commit temporaneo per aggiornamento Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,44 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavigationComponent } from './navigation/navigation.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { NotesComponent } from './notes/notes.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import {Router, RouterModule, Routes} from '@angular/router';
+
+const appRoutes :Routes = [
+  {
+    path: 'notes',
+    component: NotesComponent
+  },/*percorso che si riferisce all'url che digito nel browser ed il componente che viene utilizzato quando viene raggiunto quell'url*/
+  {
+    path: 'feedback',
+    component: FeedbackComponent
+  },
+  {
+    path: '',
+    component: NotesComponent,
+    pathMatch: 'full'
+  },
+  {
+    path:'**',
+    component: NotFoundComponent
+  }
+] /*elenco di rotte*/
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    FeedbackComponent,
+    NotesComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [
     provideClientHydration()
