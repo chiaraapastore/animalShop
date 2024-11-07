32cb13575b18ccf5f180a7ff6cad7cc927ee9fdd
chiaraapastore <chiarapastoree001@gmail.com>
2024-11-07 10:32:45 +0100

    Descrizione delle modifiche

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,31 +11,25 @@ import {FormsModule} from "@angular/forms";
 import { HomeComponent } from './home/home.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { ContactComponent } from './contact/contact.component';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { ProductListComponent } from './product-list/product-list.component';
 
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    NavigationComponent,
-    FeedbackComponent,
-    AnnouncementsComponent,
-    HomeComponent,
-    AboutUsComponent,
-    ContactComponent,
-    ProductListComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    NgbModule,
-    HttpClientModule,
-    FormsModule
-  ],
-  providers: [
-    provideClientHydration()
-  ],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        NavigationComponent,
+        FeedbackComponent,
+        AnnouncementsComponent,
+        HomeComponent,
+        AboutUsComponent,
+        ContactComponent,
+        ProductListComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        NgbModule,
+        FormsModule], providers: [
+        provideClientHydration(),
+        provideHttpClient(withInterceptorsFromDi())
+    ] })
 export class AppModule { }
