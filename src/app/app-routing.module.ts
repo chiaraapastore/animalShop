f5cfc946d59654579ab2363ccdd46944850e6d04
chiaraapastore <chiarapastoree001@gmail.com>
2024-11-20 16:52:15 +0100

    Modifiche apportate

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,15 +8,15 @@ import { AboutUsComponent } from './about-us/about-us.component';
 import { ContactComponent } from './contact/contact.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { AuthGuard } from './guards/auth.guard';
-import {CartComponent} from "./cart/cart.component";
-import {PaymentComponent} from "./payment/payment.component";
-import {ConfirmPageComponent} from "./confirm-page/confirm-page.component";
-import {ProfileComponent} from "./profile/profile.component";
-import {OrdersComponent} from "./orders/orders.component";
-import {ErrorComponent} from "./error/error.component";
+import { CartComponent } from './cart/cart.component';
+import { PaymentComponent } from './payment/payment.component';
+import { ConfirmPageComponent } from './confirm-page/confirm-page.component';
+import { ProfileComponent } from './profile/profile.component';
+import { OrdersComponent } from './orders/orders.component';
+import { ErrorComponent } from './error/error.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
   { path: 'shop', component: ProductListComponent, canActivate: [AuthGuard] },
   { path: 'announcements', component: AnnouncementsComponent, canActivate: [AuthGuard] },
@@ -28,15 +28,13 @@ const routes: Routes = [
   { path: 'confirm-page', component: ConfirmPageComponent, canActivate: [AuthGuard] },
   { path: 'user-profile', component: ProfileComponent, canActivate: [AuthGuard] },
   { path: 'orders', component: OrdersComponent, canActivate: [AuthGuard] },
-  { path: 'not-authorized', component: NotFoundComponent, canActivate: [AuthGuard] },
-  { path: 'error', component: ErrorComponent, canActivate: [AuthGuard] },
-  { path: '**', redirectTo: '/error' }
+  { path: 'not-authorized', component: NotFoundComponent },
+  { path: 'error', component: ErrorComponent },
+  { path: '**', redirectTo: '/error' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { enableTracing: true })],
   exports: [RouterModule],
-  providers: [AuthGuard]
 })
 export class AppRoutingModule {}
-
