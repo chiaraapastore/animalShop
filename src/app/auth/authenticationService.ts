feb72583ec54247b522cd90525545811722aff7f
chiaraapastore <chiarapastoree001@gmail.com>
2024-11-23 12:00:05 +0100

    Modifiche apportate

--- src/app/auth/authenticationService.ts
+++ src/app/auth/authenticationService.ts
@@ -9,12 +9,12 @@ export class AuthenticationService {
   constructor(private keycloakService: KeycloakService) {}
 
 
-  async getLoggedInUser(): Promise<{ email?: string; username?: string } | null> {
+  async getLoggedInUser(): Promise<{ email?: string, username?: string } | null> {
     try {
       const isAuthenticated = await this.keycloakService.isLoggedIn();
       if (isAuthenticated) {
         const keycloakInstance = this.keycloakService.getKeycloakInstance();
-        if (keycloakInstance?.tokenParsed) {
+        if (keycloakInstance && keycloakInstance.tokenParsed) {
           const tokenParsed: any = keycloakInstance.tokenParsed;
           const email = tokenParsed.email;
           const username = tokenParsed.preferred_username;
@@ -23,7 +23,7 @@ export class AuthenticationService {
       }
       return null;
     } catch (error) {
-      console.error("Errore durante il recupero dell'utente loggato:", error);
+      console.error('Errore durante il recupero dell\'utente loggato:', error);
       return null;
     }
   }
